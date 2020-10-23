7cc92c2b8a084b685a81c472f9cc115ae2ac492f
janitbansal <janitbansal@gmail.com>
2020-10-23 15:46:07 +0530

    submit

--- src/App.js
+++ src/App.js
@@ -12,31 +12,37 @@ class App extends React.Component {
     console.log(this.state,"state");
   }
   onMove = (event) => {
-    console.log(event , " ab move karo",event.target.tabIndex);
     let obj = document.getElementById(""+event.target.tabIndex)
+    switch(event.code){
+      case "KeyW":
+        let W_val = parseInt(obj.style.top?obj.style.top:0, 10);
+        obj.style.top = (W_val>11?(W_val - 10):0) + "px";
+        break;
+      case "KeyS":
+        let S_val = parseInt(obj.style.top?obj.style.top:0, 10);
+        obj.style.top = (S_val<460?(S_val + 10):470) + "px";
+        break;
 
-    if(event.code==="KeyW"){
-      let Y_val = parseInt(obj.style.top?obj.style.top:0, 10);
-      obj.style.top = (Y_val>11?(Y_val - 10):0) + "px";
+      case "KeyD":
+        let D_val = parseInt(obj.style.left?obj.style.left:0, 10);
+        obj.style.left = (D_val<1040?(D_val + 10):1050) + "px";
+        break;
+        
+      case "KeyA":
+        let A_val = parseInt(obj.style.left?obj.style.left:0, 10);
+        obj.style.left = (A_val>11?(A_val - 10):0) + "px";
+        break;
 
-    }else if(event.code==="KeyS"){
-      let Y_val = parseInt(obj.style.top?obj.style.top:0, 10);
-      obj.style.top = (Y_val<460?(Y_val + 10):470) + "px";
-
-    }else if(event.code==="KeyD"){
-      let X_val = parseInt(obj.style.left?obj.style.left:0, 10);
-      obj.style.left = (X_val<1040?(X_val + 10):1050) + "px";
-      
-    }else if(event.code==="KeyA"){
-      let X_val = parseInt(obj.style.left?obj.style.left:0, 10);
-      obj.style.left = (X_val>11?(X_val - 10):0) + "px";
-    }else if(event.code==="Backspace"){
-      for(let i=0;i<this.state.boxes.length;i++){
-        if(event.target.tabIndex==this.state.boxes[i].id){
-          this.state.boxes.splice(i,1)
-          this.setState(this.state);
+      case "Backspace":
+        for(let i=0;i<this.state.boxes.length;i++){
+          if(event.target.tabIndex===this.state.boxes[i].id){
+            this.state.boxes.splice(i,1)
+            this.setState(this.state);
+          }
         }
-      }
+        break;
+      default:
+        break;
     }
 
   };
